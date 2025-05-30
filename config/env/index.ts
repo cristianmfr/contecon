5755e9995301d14618ce269ded36d2a1836de48a
Cristian Freitas <crismdf2003@gmail.com>
2025-05-30 16:01:17 -0300

    fix: env

--- config/env/index.ts
+++ config/env/index.ts
@@ -2,11 +2,17 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
-  server: {
-    DATABASE_URL: z.string().url(),
-  },
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
-  emptyStringAsUndefined: true,
+	server: {
+		DATABASE_URL: z.string().url(),
+	},
+	shared: {
+		NEXT_PUBLIC_GRAPHQL_URL: z.string().url(),
+		NEXT_PUBLIC_WS_URL: z.string().url(),
+	},
+	runtimeEnv: {
+		DATABASE_URL: process.env.DATABASE_URL,
+		NEXT_PUBLIC_GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+		NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+	},
+	emptyStringAsUndefined: true,
 })
