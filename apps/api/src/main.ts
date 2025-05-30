0ed81adc8507d60eb552c11782a9c67679d3cbc8
Cristian Freitas <crismdf2003@gmail.com>
2025-05-30 15:16:11 -0300

    fix: build json

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: false,
-  })
+	const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+	app.enableCors()
 
-  await app.listen(process.env.PORT || 3333)
+	await app.listen(process.env.PORT || 3333)
 }
 bootstrap()
