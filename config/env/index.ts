d2fc60c6a923aac5cf50557efd46e267a158d164
Cristian Freitas <crismdf2003@gmail.com>
2025-05-30 16:13:34 -0300

    fix: env url required

--- config/env/index.ts
+++ config/env/index.ts
@@ -2,15 +2,11 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
-	server: {
-		DATABASE_URL: z.string().url(),
-	},
 	shared: {
 		NEXT_PUBLIC_GRAPHQL_URL: z.string(),
 		NEXT_PUBLIC_WS_URL: z.string(),
 	},
 	runtimeEnv: {
-		DATABASE_URL: process.env.DATABASE_URL,
 		NEXT_PUBLIC_GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL,
 		NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
 	},
